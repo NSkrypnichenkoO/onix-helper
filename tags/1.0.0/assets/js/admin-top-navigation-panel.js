28802e10a757e7b9c24adc50de667f72bde32e8c
Anastasiia Skrypnichenko <anastasiia.skrypnichenko@onix-systems.com>
2023-04-03 22:58:40 +0300

    Sync with svn

--- tags/1.0.0/assets/js/admin-top-navigation-panel.js
+++ tags/1.0.0/assets/js/admin-top-navigation-panel.js
@@ -2,21 +2,22 @@ document.addEventListener("DOMContentLoaded", ready);
 
 function ready() {
   let switcher = document.querySelector('#oh-description-switcher');
+  if (switcher) {
+    let checkbox = switcher.querySelector("input[type=checkbox]");
 
-  let checkbox = switcher.querySelector("input[type=checkbox]");
+    checkbox.addEventListener('change', function () {
 
-  checkbox.addEventListener('change', function() {
+      let descriptions_list = document.querySelectorAll('.onix-helper-description');
 
-    let descriptions_list = document.querySelectorAll('.onix-helper-description');
-
-    if (this.checked) {
-      descriptions_list.forEach((button) => {
-        button.classList.remove('hide-description')
-      });
-    } else {
-      descriptions_list.forEach((button) => {
-        button.classList.add('hide-description')
-      });
-    }
-  });
+      if (this.checked) {
+        descriptions_list.forEach((button) => {
+          button.classList.remove('hide-description')
+        });
+      } else {
+        descriptions_list.forEach((button) => {
+          button.classList.add('hide-description')
+        });
+      }
+    });
+  }
 }
