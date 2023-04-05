a9444ddc3fc69507f5f153ddd824941798124a74
Anastasiia Skrypnichenko <anastasiia.skrypnichenko@onix-systems.com>
2023-04-05 21:03:45 +0300

    version 1.0.1 test update

---
+++ tags/1.0.1/templates/template-parts/sections/header-main.php
@@ -0,0 +1,17 @@
+<div class="omb-wrap">
+  <?php
+
+  use Onixhelper\System\OhelperBaseController;
+
+  $base_controller = new OhelperBaseController();
+
+  settings_errors();
+
+  //require_once $base_controller->omb_path . 'templates/template-parts/holidays-decor/cat.php';
+  ?>
+  <div class="first-screen">
+    <?php require_once $base_controller->omb_path . 'templates/template-parts/functional-parts/plugin-title.php'; ?>
+    <div class="header-image">
+      <img src="<?php echo esc_url($base_controller->omb_url) . 'assets/img/mascot.png' ?>" alt="Italian Trulli">
+    </div>
+  </div>
